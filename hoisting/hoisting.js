15b51d933750e9afeb9e459ef1e37d280477e521
cjreysayago <rey933@gmail.com>
2021-06-30 20:34:00 -0300

    Fix hoisting example

--- hoisting/hoisting.js
+++ hoisting/hoisting.js
@@ -1,15 +1,10 @@
 // Hoisting in action
-/**
- * Is like having
- * var message = "Mayday!"
- */
-message = "We are safe over this ship";
 function askForHelp() {
-  var message; // Go to the top <..>/
-  message = "Mayday!"; 
+  message = "Mayday!";
+  console.log(message);
+  var message; // Declaration is hoisted to the top <..>/
 }
 askForHelp();
-console.log(message); // It will outputs "We are safe over this ship" due var message declaration was moved to the top
 
 //---------------------------
 // Error example
@@ -23,6 +18,6 @@ class Client {
 }
 const client = new Client();
 console.log("Beep!");
-console.log(response); // response is not available here, due inizialitations are not hoisted
+console.log("Response is: ", response); // undefined
 var response = client.get("https://google.com");
-console.log(response); // Now is available
+console.log("Now response is: ", response); // Now is available
